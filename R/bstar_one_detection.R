534451ebc6c1e272d259c53354f1a36146bb422a
skydavis435 <dmayer2@gmail.com>
2018-05-21 17:27:39 -0600

    load data

--- R/bstar_one_detection.R
+++ R/bstar_one_detection.R
@@ -4,4 +4,12 @@
   require(parallel)
   require(ggplot2)
   require(ggthemes)
+#Setup
+  working.directory<-file.path(path.expand("~"),"Documents","BSTAR_one_detection")
+  radar.data<-file.path(path.expand("~"),"Documents", "Radar_Data","BSTAR_V2")
+  numCores<-detectCores()
+#Load the radar data.
+  setwd(radar.data)
+  filelist<-list.files(pattern="\\.csv$")
+  datalist<-mclapply(filelist,fread,mc.cores = numCores) #faster if loading multiple CSV files    
   
